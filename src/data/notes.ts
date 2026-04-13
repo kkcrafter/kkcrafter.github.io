654f93399c8a7474abf88e9dab088473e61930e8
KK <kelvinkwrk01@gmail.com>
2026-04-13 06:03:07 +0200

    Add LkSG note

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -9,6 +9,14 @@ export interface Note {
 
 export const notes: Note[] = [
   {
+    title: 'DPP 101',
+    description: 'Core concepts about Digital Product Passport (DPP)',
+    tag: 'Sustainability',
+    url: 'https://app.heptabase.com/p/whiteboard/368c2406-0c36-4223-ae5f-fbf76a3def1d',
+    color: 'green',
+    icon: '\u{1F331}',
+  },
+  {
     title: 'GIS 101',
     description: 'Core concepts about Geoinformation System (GIS)',
     tag: 'Sustainability',
@@ -25,11 +33,11 @@ export const notes: Note[] = [
     icon: '\u{1F331}',
   },
   {
-    title: 'DPP 101',
-    description: 'Core concepts about Digital Product Passport (DPP)',
+    title: 'LkSG 101',
+    description: 'Core concepts about German Supply Chain Due Diligence Act / Lieferkettensorgfaltspflichtengesetz (LkSG)',
     tag: 'Sustainability',
-    url: 'https://app.heptabase.com/p/whiteboard/368c2406-0c36-4223-ae5f-fbf76a3def1d',
+    url: 'https://app.heptabase.com/p/whiteboard/1a44868e-920b-4952-bf44-19e2223caa41',
     color: 'green',
     icon: '\u{1F331}',
-  }
+  },
 ];
